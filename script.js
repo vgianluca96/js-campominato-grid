50412cee9238de7863a2d2a8de038d6c0980756e
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-01 14:28:54 +0200

    bottone e griglia
    Creato bottone e creata logica js per stampare la griglia

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 
 
-console.log('ciao')
+// Variabile agganciata al grid container
+let gridContainer = document.getElementById('gridCont');
+
+
+for (let i = 1; i <= 100; i++) {
+    
+    // creazione singola cella
+    let gridCell = document.createElement('div');
+    gridCell.classList.add('gridCell');
+    // Aggiungo la cella al grid container
+    gridContainer.append(gridCell);
+
+}
+
+
