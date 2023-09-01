070fb2f1f1b52328c25eb28687830c97a0fe574f
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-01 16:45:03 +0200

    bonus
    campo minato versione pepe the frog

---
+++ bonus/script.js
@@ -0,0 +1,72 @@
+
+
+// Variabile agganciata al div 'gridCont'
+let gridContainer = document.getElementById('gridCont');
+// Variabile agganciata al button
+let btnGridGen = document.getElementById('btnGridGen');
+// Scelgo 10 numeri random e stabilisco le celle con pepe the frog
+let randomNums = Array.from({length: 10}, () => Math.floor(Math.random() * 100));
+let randomCellNums = [];
+for (let i= 0; i < randomNums.length; i++) {
+    randomCellNums[i] = ('cellNum' + randomNums[i]);
+}
+console.log(randomNums);
+console.log(randomCellNums);
+
+// Event listener del button
+btnGridGen.addEventListener('click', function(){
+    
+    // Cancello tutti i figli di 'gridCont', così al click si azzera la griglia
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    // Aggiungo celle a 'gridCont'
+    for (let i = 1; i <= 100; i++) {
+
+        // creazione singola cella
+        let gridCell = document.createElement('div');
+        gridCell.classList.add('gridCell');
+        gridCell.classList.add(('cellNum' + i));
+        gridCell.innerHTML = i;
+        // Aggiungo la cella a 'gridCont'
+        gridContainer.append(gridCell);
+
+        //Aggiungo azione al click sulla cella
+        gridCell.addEventListener('click', function() {
+
+            //console.log(gridCell.classList[1]);
+
+            if (randomCellNums.includes(gridCell.classList[1])) {
+
+                gridCell.removeChild(gridCell.firstChild);
+                let newHTML = `<img src="./img/pepe-the-frog.webp" alt="">`;
+                gridCell.insertAdjacentHTML('beforeend',newHTML);
+    
+                let youLooseDiv = `<div class="youloose">HAI PERSO</div>`;
+                gridContainer.insertAdjacentHTML('beforeend',youLooseDiv);
+                //gridCell.classList.add(('cellNum' + i));
+
+            } else {
+
+                gridCell.classList.add('gridCellClick');
+                console.log('Hai cliccato sulla cella ' + gridCell.innerHTML);
+
+            }
+
+        })
+
+    }
+
+})
+
+
+/*
+function youLoose () {
+
+    for (let i = 0; i < randomCellNums.length; i++) {
+
+    }
+
+}
+*/
